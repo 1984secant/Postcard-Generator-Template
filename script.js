38fecb73505e68569c6c03d823aa876f89a7bd31
1984secant <127aacrooms@frhsd.com>
2025-11-10 12:52:25 -0500

    message

---
+++ script.js
@@ -0,0 +1,39 @@
+var stateName = document.getElementById('#states').names.length;
+
+//stateName.addEventListener("DOMContentLoaded", function);
+
+
+//when a state is selected,
+
+//greeting
+function names() {
+    var greeting = document.querySelector('#greeting');
+//where the state name will be
+var heading = document.querySelector('#state-heading');
+greeting.innerHTML = "Greetings from";
+heading.innerHTML = stateName;
+}
+// var greeting = document.querySelector('#greeting');
+// //where the state name will be
+// var heading = document.querySelector('#state-heading');
+// greeting.innerHTML = "Greetings from";
+// heading.innerHTML = stateName;
+
+console.log(stateName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//by god im confused
