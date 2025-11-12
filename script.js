3f9661e609f4813643a32eeb04b56f7efd81a24d
1984secant <127aacrooms@frhsd.com>
2025-11-12 14:05:04 -0500

    updatetca

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-var stateName = document.getElementById('#states').names.length;
-
-//stateName.addEventListener("DOMContentLoaded", function);
+// old code 
 
 
-//when a state is selected,
+//var stateName = document.getElementById('#states').names.length;
 
-//greeting
-function names() {
-    var greeting = document.querySelector('#greeting');
-//where the state name will be
-var heading = document.querySelector('#state-heading');
-greeting.innerHTML = "Greetings from";
-heading.innerHTML = stateName;
-}
-// var greeting = document.querySelector('#greeting');
+// //stateName.addEventListener("DOMContentLoaded", function);
+
+
+// //when a state is selected,
+
+// //greeting
+// function names() {
+//     var greeting = document.querySelector('#greeting');
 // //where the state name will be
 // var heading = document.querySelector('#state-heading');
 // greeting.innerHTML = "Greetings from";
 // heading.innerHTML = stateName;
+// }
+// // var greeting = document.querySelector('#greeting');
+// // //where the state name will be
+// // var heading = document.querySelector('#state-heading');
+// // greeting.innerHTML = "Greetings from";
+// // heading.innerHTML = stateName;
 
-console.log(stateName);
+// console.log(stateName);
+
+let gr = document.getElementById("greeting");
+let stn = document.getElementById("stateName");
+let post = document.getElementById("postcard");
+states.addEventListener("change", newState);
+
+function newState() {
+    var picked = document.getElementById("states");
+    gr.innerHTML = "Greetings from";
+    stn.innerHTML = "" + picked;
+   post.style.backgroundImage = "url('../img/nj@2x.jpg')";
+    console.log(picked);
+}
 
 
 
@@ -31,9 +47,4 @@ console.log(stateName);
 
 
 
-
-
-
-
-
-//by god im confused
+//what the mess
